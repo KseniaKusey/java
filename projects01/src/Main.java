fabcfe05daf9e555af846d9819908abe4551ab01
127_Kusey_Ksenia <113189436+KseniaKusey@users.noreply.github.com>
2025-06-24 21:54:00 +0300

    Refueling

---
+++ projects01/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main (String[] atgs) {
+        System.out.println("Система расчета стоимости топлива");
+
+         int fuelTipe = 95;
+         int amount = 50 ;
+
+         double fuel92price = 60.2;
+         double fuel95price = 67.33;
+
+         double fuelPrice = 0;
+         if(fuelTipe == 92) {
+             fuelPrice = fuel92price;
+         } else if (fuelTipe == 95) {
+            fuelPrice = fuel95price;
+         } else {
+             System.out.println("Указан неверный тип топлива");
+         }
+
+         if(amount < 1) {
+             System.out.println("Казано слишком малое количества топлива");
+             amount = 0;
+         }
+
+        System.out.println("Цена выбранного топлива: " + fuelPrice +" руб");
+
+        double totalPrice = fuelPrice * amount;
+        System.out.println("Общая стоимость заправки: " + totalPrice + " руб");
+
+    }
+}
