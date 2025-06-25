2d7b804951d34edb7da61810a17d99ff1e29c1e3
127_Kusey_Ksenia <113189436+KseniaKusey@users.noreply.github.com>
2025-06-25 13:08:09 +0300

    Refueling

--- projects01/src/Main.java
+++ projects01/src/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
          int fuelTipe = 95;
          int amount = 50 ;
+         boolean hasDiscount = amount > 10;
 
          double fuel92price = 60.2;
          double fuel95price = 67.33;
+
+         double discount = 0.1;
 
          double fuelPrice = 0;
          if(fuelTipe == 92) {
@@ -15,6 +18,10 @@ public class Main {
             fuelPrice = fuel95price;
          } else {
              System.out.println("Указан неверный тип топлива");
+         }
+
+         if(hasDiscount) {
+             fuelPrice = (1 -discount) * fuelPrice;
          }
 
          if(amount < 1) {
